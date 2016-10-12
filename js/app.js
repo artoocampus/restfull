abb5afd6f964f4221b1ef92dc16f2963e2f15a1a
artoocampus <paolo.calvaresi.83@gmail.com>
2016-10-12 17:17:37 +0200

    add event on button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 
 
+    var root = 'http://www.omdbapi.com/';
+
+    $("#buttonSearch").click(function() {
+        var title = $("#title").val();
+
+       
+
+    });
+
 });
