ca8cd8924c5b0cd54a836d75ec25bdedcc92d5c2
artoocampus <paolo.calvaresi.83@gmail.com>
2016-10-12 17:55:11 +0200

    add validation and print results

--- js/app.js
+++ js/app.js
@@ -2,7 +2,26 @@ $(document).ready(function() {
 
 
     var root = 'http://www.omdbapi.com/';
+    var listOfMovies = [];
+    var totalResults;
 
+    //DISABILITA IL BOTTONE DI RICERCA
+    $("#buttonSearch").prop('disabled', true);
+
+    //VERIFICA CHE IL CAMPO TITLE NON SIA VUOTO
+    $("#title").change(function() {
+        var validate = false;
+        if ($("#title").val().length > 0) {
+            validate = true;
+        }
+        if (validate) {
+            $("#buttonSearch").prop('disabled', false);
+        } else {
+            $("#buttonSearch").prop('disabled', true);
+        }
+    });
+
+    //TRIGGER DELL' EVENTO CLICK
     $("#buttonSearch").click(function() {
         var title = $("#title").val();
 
@@ -10,7 +29,15 @@ $(document).ready(function() {
             url: root + '?s=' + title,
             method: 'GET'
         }).then(function(data) {
-            console.log(data);
+            
+            if (data.Response === "True") {
+                totalResults = data.totalResults;
+                listOfMovies = data.Search;
+                $("#result").html(totalResults);
+            }
+            if(data.Response === "False") {
+            	 $("#result").html(data.Error).css('color', 'red');
+            }
         });
 
     });
