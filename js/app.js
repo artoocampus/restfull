ce6b18b220737686af1849535ab1b9a420523da2
artoocampus <paolo.calvaresi.83@gmail.com>
2016-10-16 17:57:27 +0200

    add detail service

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-    var root = 'http://www.omdbapi.com/';
+
     var listOfMovies = [];
     var totalResults;
     $('.listaMovies').hide();
@@ -32,25 +32,23 @@ $(document).ready(function() {
         var genre = $("#genre").val();
 
         //CHIAMATA AJAX AL SERVIZIO OMDB
-        $.ajax({
-            url: root + '?s=' + title + '&type=' + genre,
-            method: 'GET'
-        }).then(function(data) {
-            if (data.Response === "True") {
-                totalResults = data.totalResults;
-                listOfMovies = data.Search;
-                $("#resultError").hide();
-                $("#result").html("Risultati: " + totalResults).css('color', 'black');
-                $('.dettaglio').hide();
-                $('.listaMovies').show();
-                drawTable(listOfMovies);
-            }
-            if (data.Response === "False") {
-                $('.listaMovies').hide();
-                $('.dettaglio').hide();
-                $("#resultError").html(data.Error).css('color', 'red').show();
-            }
-        });
+        moviesService.getList(title, genre)
+            .then(function(data) {
+                if (data.Response === "True") {
+                    totalResults = data.totalResults;
+                    listOfMovies = data.Search;
+                    $("#resultError").hide();
+                    $("#result").html("Risultati: " + totalResults).css('color', 'black');
+                    $('.dettaglio').hide();
+                    $('.listaMovies').show();
+                    drawTable(listOfMovies);
+                }
+                if (data.Response === "False") {
+                    $('.listaMovies').hide();
+                    $('.dettaglio').hide();
+                    $("#resultError").html(data.Error).css('color', 'red').show();
+                }
+            });
 
     });
 
@@ -84,84 +82,78 @@ $(document).ready(function() {
         var titleMovie = $(this).attr("title");
 
         //CHIAMATA AJAX AL DETTAGLIO DEL FILM
-        $.ajax({
-            url: root + '?t=' + titleMovie,
-            method: 'GET'
-        }).then(function(data) {
-            if (data.Response === "True") {
-                $("#resultError").hide();
-                $('.listaMovies').hide();
-                $('.dettaglio').show();
-                drawDettaglio(data);
-            }
-            if (data.Response === "False") {
-                $("#resultError").hide();
-                $('.listaMovies').hide();
-                $('.dettaglio').hide();
-            }
-        });
+        moviesService.getMovie(titleMovie)
+            .then(function(data) {
+                if (data.Response === "True") {
+                    $("#resultError").hide();
+                    $('.listaMovies').hide();
+                    $('.dettaglio').show();
+                    drawDettaglio(data);
+                }
+                if (data.Response === "False") {
+                    $("#resultError").hide();
+                    $('.listaMovies').hide();
+                    $('.dettaglio').hide();
+                }
+            });
     });
 
     //DRAW DETTAGLIO
     function drawDettaglio(dettaglio) {
         $('#titoloFilm').html(dettaglio.Title);
         if (dettaglio.Poster != 'N/A') {
-        $('#poster').html('<img width="200x350" src="' + dettaglio.Poster + '"/>');
-    } else {
-        $('#poster').html('<img width="200x350" src="http://placehold.it/200x350"/>');
+            $('#poster').html('<img width="200x350" src="' + dettaglio.Poster + '"/>');
+        } else {
+            $('#poster').html('<img width="200x350" src="http://placehold.it/200x350"/>');
+        }
+
+        $('#lista1 li').remove();
+        $('#lista2 li').remove();
+        $('#cast li').remove();
+
+        $('#lista1')
+            .append('<li><strong>Rating</strong> :' + dettaglio.imdbRating + '/10 ' + drawStar(dettaglio.imdbRating) + '</li>')
+            .append('<li><strong>Year</strong> :' + dettaglio.Year + '</li>')
+            .append('<li><strong>Director</strong> :' + dettaglio.Director + '</li>')
+            .append('<li><strong>Writer</strong> :' + dettaglio.Writer + '</li>');
+        $('#lista2')
+            .append('<li><strong>Country</strong> :' + dettaglio.Country + '</li>')
+            .append('<li><strong>Genre</strong> :' + dettaglio.Genre + '</li>')
+            .append('<li><strong>Language</strong> :' + dettaglio.Language + '</li>')
+            .append('<li><strong>Released</strong> :' + dettaglio.Released + '</li>');
+        $('#awards').html(dettaglio.Awards);
+        $('#plot').html(dettaglio.Plot);
+
+        var attori = listMe(dettaglio.Actors);
+
+        for (attore of attori) {
+            $('#cast').append('<li>' + attore + '</li>');
+
+        }
+
     }
 
-    $('#lista1 li').remove();
-    $('#lista2 li').remove();
-    $('#cast li').remove();
 
-    $('#lista1')
-        .append('<li><strong>Rating</strong> :' + dettaglio.imdbRating + '/10 ' + drawStar(dettaglio.imdbRating) + '</li>')
-        .append('<li><strong>Year</strong> :' + dettaglio.Year + '</li>')
-        .append('<li><strong>Director</strong> :' + dettaglio.Director + '</li>')
-        .append('<li><strong>Writer</strong> :' + dettaglio.Writer + '</li>');
-    $('#lista2')
-        .append('<li><strong>Country</strong> :' + dettaglio.Country + '</li>')
-        .append('<li><strong>Genre</strong> :' + dettaglio.Genre + '</li>')
-        .append('<li><strong>Language</strong> :' + dettaglio.Language + '</li>')
-        .append('<li><strong>Released</strong> :' + dettaglio.Released + '</li>');
-    $('#awards').html(dettaglio.Awards);
-    $('#plot').html(dettaglio.Plot);
-
-    var attori = listMe(dettaglio.Actors);
-
-    for (attore of attori) {
-        $('#cast').append('<li>' + attore + '</li>');
-
+    //FUNZIONE PER AVERE UN ARRAY DI ATTORI DA UNA STRINGA
+    function listMe(Stringactors) {
+        var actors = Stringactors.split(",");
+        var newActors = [];
+        actors.forEach(function(el) {
+            newActors.push(el.trim());
+        });
+        return newActors;
     }
 
+    //FUNZIONE DRAWSTAR 
+    function drawStar(rate) {
+        var rating = '';
+        rate = Math.floor(rate);
 
-}
+        for (i = 0; i < rate; i++) {
+            rating += '<span class="glyphicon glyphicon-star" style="color:#a98605"></span>';
+        }
 
-
-//FUNZIONE PER AVERE UN ARRAY DI ATTORI DA UNA STRINGA
-function listMe(Stringactors) {
-    var actors = Stringactors.split(",");
-    var newActors = [];
-    actors.forEach(function(el) {
-        newActors.push(el.trim());
-    });
-    return newActors;
-}
-
-//FUNZIONE DRAWSTAR 
-function drawStar(rate) {
-    var rating = '';
-    rate = Math.floor(rate);
-
-    for (i = 0; i < rate; i++) {
-        rating += '<span class="glyphicon glyphicon-star" style="color:#a98605"></span>';
+        return rating;
     }
-
-    return rating;
-}
-
-
-console.log(service.getname());
 
 });
